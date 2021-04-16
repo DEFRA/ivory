cc26812acbe3fe152f4efc4cd9a407baeb7efb91
Duncan Nixon <duncannixon@me.com>
2021-04-16 08:17:23 +0100

    Removed unnecessary paths.

--- server/utils/constants.js
+++ server/utils/constants.js
@@ -2,11 +2,9 @@ const Constants = (module.exports = {})
 
 Constants.Paths = {
   CHECK_YOUR_ANSWERS: 'check-your-answers',
-  HOME: 'home',
   IVORY_ADDED: 'ivory-added',
   IVORY_INTEGRAL: 'ivory-integral',
-  TAKEN_FROM_ELEMENT: 'taken-from-elephant',
-  YES_NO_IDK: 'yes-no-idk'
+  TAKEN_FROM_ELEMENT: 'taken-from-elephant'
 }
 
 Constants.Views = {
