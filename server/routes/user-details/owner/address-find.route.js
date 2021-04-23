985d294b1254dc4d3bf11ea8b0c376b0b1303f2d
Duncan Nixon <duncannixon@me.com>
2021-04-23 10:19:38 +0100

    Refactored to use form-layout, added IDs

--- server/routes/user-details/owner/address-find.route.js
+++ server/routes/user-details/owner/address-find.route.js
@@ -1,8 +1,8 @@
 'use strict'
 
-const { postcodeValidator } = require('postcode-validator')
 const { Paths, Views } = require('../../../utils/constants')
 const AddressService = require('../../../services/address.service')
+const { buildErrorSummary, Validators } = require('../../../utils/validation')
 
 const completedBy = 'owner' // Temporary until previous page built then will use value saved in Redis. Use 'owner' or '3rdParty'
 
@@ -14,31 +14,24 @@ const handlers = {
   },
   post: async (request, h) => {
     const payload = request.payload
+    const errors = _validateForm(payload)
 
-    // If no postcode entered
-    if (!payload.postcode) {
-      return h.view(Views.ADDRESS_FIND, {
-        ..._getContext,
-        errorSummaryText: completedBy === 'owner' ? 'Enter your postcode' : 'Enter the owner’s postcode',
-        errorText: {
-          text: completedBy === 'owner' ? 'Enter your postcode' : 'Enter the owner’s postcode'
-        }
-      })
-    // If an invalid postcode entered
-    } else if (!postcodeValidator(payload.postcode, 'GB')) {
-      return h.view(Views.ADDRESS_FIND, {
-        ..._getContext,
-        errorSummaryText: 'Enter a UK postcode in the correct format',
-        errorText: {
-          text: 'Enter a UK postcode in the correct format'
-        }
-      })
-    } else {
-      const test = await AddressService.addressSearch(payload.nameNumber, payload.postcode)
-      console.log(test)
-      const address = test.results
-      return (address)
+    if (errors.length) {
+      return h
+        .view(Views.ADDRESS_FIND, {
+          ..._getContext(),
+          ...buildErrorSummary(errors)
+        })
+        .code(400)
     }
+
+    const test = await AddressService.addressSearch(
+      payload.nameNumber,
+      payload.postcode
+    )
+    console.log(test)
+    const address = test.results
+    return address
   }
 }
 
@@ -46,22 +39,50 @@ const _getContext = () => {
   if (completedBy === 'owner') {
     return {
       title: 'What is your address?',
-      helpText: 'If your business is the legal owner of the item, give your business address.'
+      helpText:
+        'If your business is the legal owner of the item, give your business address.'
     }
   } else {
     return {
       title: 'What is the owner’s address?',
-      helpText: 'If the legal owner of the item is a business, give the business address.'
+      helpText:
+        'If the legal owner of the item is a business, give the business address.'
     }
   }
 }
 
-module.exports = [{
-  method: 'GET',
-  path: `${Paths.OWNER_ADDRESS_FIND}`,
-  handler: handlers.get
-}, {
-  method: 'POST',
-  path: `${Paths.OWNER_ADDRESS_FIND}`,
-  handler: handlers.post
-}]
+const _validateForm = payload => {
+  const errors = []
+
+  if (Validators.empty(payload.postcode)) {
+    errors.push({
+      name: 'postcode',
+      text:
+        completedBy === 'owner'
+          ? 'Enter your postcode'
+          : 'Enter the owner’s postcode'
+    })
+  }
+
+  if (!Validators.postcode(payload.postcode)) {
+    errors.push({
+      name: 'postcode',
+      text: 'Enter a UK postcode in the correct format'
+    })
+  }
+
+  return errors
+}
+
+module.exports = [
+  {
+    method: 'GET',
+    path: `${Paths.OWNER_ADDRESS_FIND}`,
+    handler: handlers.get
+  },
+  {
+    method: 'POST',
+    path: `${Paths.OWNER_ADDRESS_FIND}`,
+    handler: handlers.post
+  }
+]
