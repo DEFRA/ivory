8ba2d86106997e0399135ccda0b56f0fe0fa83fa
Duncan Nixon <duncannixon@me.com>
2021-04-26 10:55:19 +0100

    Refactored address service to query for multiple pages of data. Added stubs for follow-on routes.

---
+++ server/routes/user-details/owner/address-confirm.route.js
@@ -0,0 +1,43 @@
+'use strict'
+
+const { Paths, Views } = require('../../../utils/constants')
+
+const handlers = {
+  get: (request, h) => {
+    return h.view(Views.ADDRESS_CONFIRM, {
+      ..._getContext()
+    })
+  },
+  post: async (request, h) => {
+    return h.view(Views.ADDRESS_OUTSIDE_UK)
+  }
+}
+
+const _getContext = () => {
+  // if (completedBy === 'owner') {
+  //   return {
+  //     title: 'What is your address?',
+  //     helpText:
+  //       'If your business is the legal owner of the item, give your business address.'
+  //   }
+  // } else {
+  //   return {
+  //     title: 'What is the owner’s address?',
+  //     helpText:
+  //       'If the legal owner of the item is a business, give the business address.'
+  //   }
+  // }
+}
+
+module.exports = [
+  {
+    method: 'GET',
+    path: `${Paths.OWNER_ADDRESS_CONFIRM}`,
+    handler: handlers.get
+  },
+  {
+    method: 'POST',
+    path: `${Paths.OWNER_ADDRESS_CONFIRM}`,
+    handler: handlers.post
+  }
+]
---
+++ server/routes/user-details/owner/address-enter.route.js
@@ -0,0 +1,81 @@
+'use strict'
+
+const { Paths, Views } = require('../../../utils/constants')
+const { buildErrorSummary } = require('../../../utils/validation')
+
+// const completedBy = 'owner' // Temporary until previous page built then will use value saved in Redis. Use 'owner' or '3rdParty'
+
+const handlers = {
+  get: (request, h) => {
+    return h.view(Views.ADDRESS_ENTER, {
+      ..._getContext()
+    })
+  },
+  post: async (request, h) => {
+    const payload = request.payload
+    const errors = _validateForm(payload)
+
+    if (errors.length) {
+      return h
+        .view(Views.ADDRESS_ENTER, {
+          ..._getContext(),
+          ...buildErrorSummary(errors)
+        })
+        .code(400)
+    }
+
+    return h.view(Views.ADDRESS_OUTSIDE_UK)
+  }
+}
+
+const _getContext = () => {
+  // if (completedBy === 'owner') {
+  //   return {
+  //     title: 'What is your address?',
+  //     helpText:
+  //       'If your business is the legal owner of the item, give your business address.'
+  //   }
+  // } else {
+  //   return {
+  //     title: 'What is the owner’s address?',
+  //     helpText:
+  //       'If the legal owner of the item is a business, give the business address.'
+  //   }
+  // }
+}
+
+const _validateForm = payload => {
+  const errors = []
+
+  // if (Validators.empty(payload.postcode)) {
+  //   errors.push({
+  //     name: 'postcode',
+  //     text:
+  //       completedBy === 'owner'
+  //         ? 'Enter your postcode'
+  //         : 'Enter the owner’s postcode'
+  //   })
+  // }
+
+  // if (!Validators.postcode(payload.postcode)) {
+  //   errors.push({
+  //     name: 'postcode',
+  //     text: 'Enter a UK postcode in the correct format'
+  //   })
+  // }
+
+  return errors
+}
+
+module.exports = [
+  {
+    method: 'GET',
+    path: `${Paths.OWNER_ADDRESS_ENTER}`,
+    handler: handlers.get
+  },
+  {
+    method: 'POST',
+    path: `${Paths.OWNER_ADDRESS_ENTER}`,
+    handler: handlers.post
+  }
+]
