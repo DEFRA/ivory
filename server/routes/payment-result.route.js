61ca8e9f3b1731580d3878220244b6086fa893c5
Duncan Nixon <duncannixon@me.com>
2021-05-11 13:59:48 +0100

    Debugging (WIP)

--- server/routes/payment-result.route.js
+++ server/routes/payment-result.route.js
@@ -2,15 +2,24 @@
 
 const { Paths, Views, RedisKeys } = require('../utils/constants')
 // const PaymentService = require('../services/payment.service')
-const RedisService = require('../services/redis.service')
+// const RedisService = require('../services/redis.service')
+
+// const server = require('../../server/index')
 
 const handlers = {
   get: async (request, h) => {
-    const paymentId = await RedisService.get(request, RedisKeys.PAYMENT_ID)
-    console.log(paymentId)
-    return h.view(Views.SERVICE_COMPLETE, {
-      ..._getContext()
-    })
+    // const paymentId = await RedisService.get(request, RedisKeys.PAYMENT_ID)
+    // const paymentId = await h.state.sessionId
+    // console.log(paymentId)
+
+    // const paymentId2 = await request.state.sessionId
+    // console.log(paymentId2)
+
+    // console.log(server.state)
+    // return h.view(Views.SERVICE_COMPLETE, {
+    //   ..._getContext()
+    // })
+    return h.redirect(Paths.SERVICE_COMPLETE)
   },
 
   post: async (request, h) => {
--- server/routes/service-complete.route.js
+++ server/routes/service-complete.route.js
@@ -3,7 +3,19 @@
 const { Paths, Views } = require('../utils/constants')
 
 const handlers = {
-  get: (request, h) => {
+  get: async (request, h) => {
+    //     const paymentId = await RedisService.get(request, RedisKeys.PAYMENT_ID)
+    // const paymentId = await h.state.sessionId
+    // console.log(paymentId)
+
+    const paymentId2 = request.state.sessionId
+    console.log(paymentId2)
+
+    // console.log(server.state)
+    // return h.view(Views.SERVICE_COMPLETE, {
+    //   ..._getContext()
+    // // })
+
     return h.view(Views.SERVICE_COMPLETE, {
       ..._getContext()
     })
