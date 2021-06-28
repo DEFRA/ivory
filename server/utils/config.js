94fa342b1eaaf152d2488defe329ab87904c19fa
Duncan Nixon <duncannixon@me.com>
2021-06-28 11:57:13 +0100

    Removed config defaults that did not appear to be working on the unit test server

--- server/utils/config.js
+++ server/utils/config.js
@@ -19,34 +19,26 @@ const schema = joi.object().keys({
     .valid(...envs)
     .default(envs[0]),
   serviceHost: joi.string(),
-  servicePort: joi.number().default(3000),
-  serviceName: joi.string().default('No service name in .env'),
+  servicePort: joi.number(),
+  serviceName: joi.string(),
   logLevel: joi.string().default('warn'),
-  requestTimeout: joi.number().default(120000),
-  maximumFileSize: joi.number().default(32),
-  redisHost: joi.string().default('127.0.0.1'),
-  redisPort: joi.number().default(6379),
+  requestTimeout: joi.number(),
+  maximumFileSize: joi.number(),
+  redisHost: joi.string(),
+  redisPort: joi.number(),
   redisPassword: joi.string(),
-  serviceApiEnabled: joi
-    .bool()
-    .valid(true, false)
-    .default(false),
-  serviceApiHost: joi.string().default('http://127.0.0.1'),
-  serviceApiPort: joi.number().default(3010),
-  addressLookupEnabled: joi
-    .bool()
-    .valid(true, false)
-    .default(false),
-  addressLookupUrl: joi.string().default('http://some-url'),
+  serviceApiEnabled: joi.bool().valid(true, false),
+  serviceApiHost: joi.string(),
+  serviceApiPort: joi.number(),
+  addressLookupEnabled: joi.bool().valid(true, false),
+  addressLookupUrl: joi.string(),
   addressLookupPassphrase: joi.string(),
   addressLookupPfxCert: joi.string(),
-  cookieValidationPassword: joi
-    .string()
-    .default('XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX'),
+  cookieValidationPassword: joi.string(),
   paymentUrl: joi.string(),
   paymentApiKey: joi.string(),
-  paymentAmountBandA: joi.number().default(2000),
-  paymentAmountBandB: joi.number().default(25000),
+  paymentAmountBandA: joi.number(),
+  paymentAmountBandB: joi.number(),
   useBasicAuth: joi.bool(),
   basicAuthPassword: joi.string()
 })
@@ -55,27 +47,29 @@ const schema = joi.object().keys({
 const config = {
   env: process.env.NODE_ENV,
   serviceHost: process.env.SERVICE_HOST,
-  servicePort: process.env.SERVICE_PORT,
-  serviceName: process.env.SERVICE_NAME,
+  servicePort: process.env.SERVICE_PORT || 3000,
+  serviceName: process.env.SERVICE_NAME || 'No service name in .env',
   logLevel: process.env.LOG_LEVEL,
-  requestTimeout: process.env.REQUEST_TIMEOUT,
-  maximumFileSize: process.env.MAXIMUM_FILE_SIZE,
-  redisHost: process.env.REDIS_HOST,
-  redisPort: process.env.REDIS_PORT,
+  requestTimeout: process.env.REQUEST_TIMEOUT || 120000,
+  maximumFileSize: process.env.MAXIMUM_FILE_SIZE || 32,
+  redisHost: process.env.REDIS_HOST || '127.0.0.1',
+  redisPort: process.env.REDIS_PORT || 6379,
   redisPassword: process.env.REDIS_PASSWORD,
   serviceApiEnabled: process.env.SERVICE_API_ENABLED,
-  serviceApiHost: process.env.SERVICE_API_HOST,
-  serviceApiPort: process.env.SERVICE_API_PORT,
-  addressLookupEnabled: process.env.ADDRESS_LOOKUP_ENABLED,
-  addressLookupUrl: process.env.ADDRESS_LOOKUP_URL,
+  serviceApiHost: process.env.SERVICE_API_HOST || 'http://127.0.0.1',
+  serviceApiPort: process.env.SERVICE_API_PORT || 3010,
+  addressLookupEnabled: process.env.ADDRESS_LOOKUP_ENABLED || false,
+  addressLookupUrl: process.env.ADDRESS_LOOKUP_URL || 'http://some-url',
   addressLookupPassphrase: process.env.ADDRESS_LOOKUP_PASSPHRASE,
   addressLookupPfxCert: process.env.ADDRESS_LOOKUP_PFX_CERT,
-  cookieValidationPassword: process.env.COOKIE_VALIDATION_PASSWORD,
+  cookieValidationPassword:
+    process.env.COOKIE_VALIDATION_PASSWORD ||
+    'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX',
   paymentUrl: process.env.PAYMENT_URL || 'http://some-url',
   paymentApiKey: process.env.PAYMENT_API_KEY || 'some-api-key',
-  paymentAmountBandA: process.env.PAYMENT_AMOUNT_BAND_A,
-  paymentAmountBandB: process.env.PAYMENT_AMOUNT_BAND_B,
-  useBasicAuth: getBoolean(process.env.USE_BASIC_AUTH || 'false'),
+  paymentAmountBandA: process.env.PAYMENT_AMOUNT_BAND_A || 2000,
+  paymentAmountBandB: process.env.PAYMENT_AMOUNT_BAND_B || 25000,
+  useBasicAuth: getBoolean(process.env.USE_BASIC_AUTH || false),
   basicAuthPassword: process.env.BASIC_AUTH_PASSWORD
 }
 
