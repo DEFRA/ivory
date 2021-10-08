46f02d8f4ef59ee9b5f9be953ba311e43b7c3a17
Jason Claxton <30830544+Jozzey@users.noreply.github.com>
2021-10-08 09:29:42 +0100

    Initial clamav setup

--- server/routes/service-status.route.js
+++ server/routes/service-status.route.js
@@ -1,6 +1,7 @@
 'use strict'
 
 const moment = require('moment')
+const NodeClam = require('clamscan')
 const nodePackage = require('../../package.json')
 
 const { Paths, Views } = require('../utils/constants')
@@ -14,6 +15,7 @@ const handlers = {
       data: {
         name: nodePackage.name,
         version: nodePackage.version,
+        clamVersion: await _getClamVersion(),
         rendered: moment(new Date()).format(DATE_FORMAT_DMY_WITH_HMS),
         uptime: moment(Date.now() - process.uptime() * 1000).format(
           DATE_FORMAT_DMY_WITH_HMS
@@ -21,6 +23,30 @@ const handlers = {
       },
       hideBackLink: true
     })
+  }
+}
+
+const _getClamVersion = async () => {
+  const options = {
+    removeInfected: false, // If true, removes infected files
+    quarantineInfected: false, // False: Don't quarantine, Path: Moves files to this place.
+    scanLog: null, // Path to a writeable log file to write scan results into
+    debugMode: false, // Whether or not to log info/debug/error msgs to the console
+    fileList: null, // path to file containing list of files to scan (for scanFiles method)
+    scanRecursively: true, // If true, deep scan folders recursively
+    clamscan: {
+      path: '/usr/bin/clamscan', // Path to clamscan binary on your server
+      db: null, // Path to a custom virus definition database
+      scanArchives: true, // If true, scan archives (ex. zip, rar, tar, dmg, iso, etc...)
+      active: true // If true, this module will consider using the clamscan binary
+    }
+  }
+
+  try {
+    const clamscan = await new NodeClam().init(options)
+    return await clamscan.getVersion()
+  } catch (err) {
+    return err
   }
 }
 
