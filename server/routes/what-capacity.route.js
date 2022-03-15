27e5743dbdf6009befcbe929188f34f01faa3faf
Duncan Nixon <duncannixon@me.com>
2022-03-15 10:28:48 +0000

    Fixed SonarCloud warning

--- server/routes/what-capacity.route.js
+++ server/routes/what-capacity.route.js
@@ -84,13 +84,11 @@ const _getOptions = whatCapacity => {
     }
   })
 
-  const items = options.map(option => ({
+  return options.map(option => ({
     text: option.label,
     value: option.label,
     checked: option.checked
   }))
-
-  return items
 }
 
 const _validateForm = payload => {
